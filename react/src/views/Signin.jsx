cc7262fdc4af191f98a1ec399669b8b7d376de12
JP Guil-an <jpguilan.official@gmail.com>
2025-05-02 22:31:18 +0800

    edited class-classname due to error

--- react/src/views/Signin.jsx
+++ react/src/views/Signin.jsx
@@ -49,7 +49,7 @@ export default function Login() {
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               {errors && errors.message && (
-                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
                   <strong className="font-bold">Error! </strong>
                   <span className="block sm:inline"> Password or Number given is invalid.</span>
                   <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
@@ -62,7 +62,7 @@ export default function Login() {
 
               <form className="space-y-4 md:space-y-6" onSubmit={onSubmit}>
                 <div>
-                  <label for="mobile_number" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Mobile Number</label>
+                  <label htmlFor="mobile_number" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Mobile Number</label>
                   <input
                     ref={mobile_numberRef}
                     id="mobile_number"
@@ -71,7 +71,7 @@ export default function Login() {
                     className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
                 </div>
                 <div>
-                  <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                  <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                   <input type="password"
                     ref={passwordRef}
                     id="password"
@@ -81,14 +81,14 @@ export default function Login() {
                     className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
                 </div>
                 <div className="flex items-center justify-between">
-                  <div className="flex items-start">
+                  {/* <div className="flex items-start">
                     <div className="flex items-center h-5">
                       <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required/>
                     </div>
                     <div className="ml-3 text-sm">
                       <label for="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
                     </div>
-                  </div>
+                  </div> */}
                   <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                 </div>
                 <button
@@ -105,7 +105,7 @@ export default function Login() {
                           disabled={isSubmitting}
                         >Sign in</button> */}
                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                  Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                  Don’t have an account yet? <a href="/signup" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
                 </p>
               </form>
             </div>
--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -142,9 +142,9 @@ const dataURLtoBlob = (dataURL) => {
 
   return (
     // <!-- ===== Page Wrapper Start ===== -->
-    <div class="flex h-screen overflow-hidden">
+    <div className="flex h-screen overflow-hidden">
       {/* <!-- ===== Content Area Start ===== --> */}
-      <div class="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+      <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
         {/* ===== Main Content Start ===== */}
         <main>
           <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
