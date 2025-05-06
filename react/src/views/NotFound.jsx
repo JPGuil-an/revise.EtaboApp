e2a9d06417b660c51db5b397708c0c6b19e90ae7
JP Guil-an <jpguilan.official@gmail.com>
2025-05-06 23:19:39 +0800

    comment only for documentation

--- react/src/views/NotFound.jsx
+++ react/src/views/NotFound.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
+
+    // this is not found page comment only
     <div className="bg-gray-800 h-screen">
       <div className="flex items-center justify-center py-12">
         <div className="bg-white border rounded-md flex items-center justify-center mx-4 md:w-2/3 ">
